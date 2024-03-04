da8a5678faaa072441d0efea6d2272bf9c9bbed5
Paul <paul@theapplegates.com>
2024-03-03 23:43:21 -0500

    tests

--- config/plugins/markdown.js
+++ config/plugins/markdown.js
@@ -46,10 +46,10 @@ const markdownLib = markdownIt({
   .use(markdownItEmoji)
   .use(markdownItEleventyImg, {
     imgOptions: {
-      widths: [440, 880, 1024],
+      widths: [220, 440, 880, 1024, 1360, 2720],
       urlPath: '/assets/images/',
       outputDir: './dist/assets/images/',
-      formats: ['webp', 'jpeg']
+      formats: ['avif', 'webp', 'jpeg']
     },
     globalAttributes: {
       loading: 'lazy',
